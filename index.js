53fdc6a102848272ed083872badb38a814748d8e
kanngji <50470748+kanngji@users.noreply.github.com>
2022-07-20 01:36:03 +0900

    boiler plate 2강 mongoouse 연결

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 const express = require("express");
 //app을 만들어야합니다
 const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb://kangji:qwe123@ac-vftwtbl-shard-00-00.cg35n6x.mongodb.net:27017,ac-vftwtbl-shard-00-01.cg35n6x.mongodb.net:27017,ac-vftwtbl-shard-00-02.cg35n6x.mongodb.net:27017/?ssl=true&replicaSet=atlas-8leupq-shard-0&authSource=admin&retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("hello world");
