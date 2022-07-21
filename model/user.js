094abd279ac838a3fb238b74e4d9a7e673a675c4
kanngji <50470748+kanngji@users.noreply.github.com>
2022-07-22 00:58:06 +0900

    model

---
+++ model/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+//schema 만들기
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0, //nomal user
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
